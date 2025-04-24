6daaba3164bb675f218e76194d8ac7172126f8b8
taiseiue <taisei@wsoft.ws>
2025-04-24 12:05:06 +0900

    全角！とかを入力できるようにした

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-ln -s $(pwd)/aquaskk ~/.config/aquaskk
 ln -s $(pwd)/brewfile ~/.config/brewfile
 ln -s $(pwd)/git ~/.config/git
 
 ln -s $(pwd)/zsh/.zshrc ~/.zshrc
 ln -s $(pwd)/zsh/.zshenv ~/.zshenv
 
-git config --global core.excludesfile ~/.config/git/gitignore 
+for file in $(pwd)/aquaskk/*; do
+  ln -s "${file}" ~/"Library/Application Support/AquaSKK/$(basename ${file})"
+done
+
+git config --global core.excludesfile ~/.config/git/gitignore
