4bd9c4e8647b49ca5ef33f732ce71a58e4bf26d1
taiseiue <taiseiue@wsnet.jp>
2025-06-01 05:29:40 +0900

    -fにした

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ export DOTFILES_ROOT="$(cd "$(dirname "$0")" && pwd)"
 echo "source $DOTFILES_ROOT/common/zsh/.zshrc" >> ~/.zshrc
 echo "source $DOTFILES_ROOT/common/zsh/.zshenv" >> ~/.zshenv
 
-ln -s "$DOTFILES_ROOT/common/git" ~/.config/git
+ln -f -s "$DOTFILES_ROOT/common/git" ~/.config/git
 git config --global core.excludesfile "$DOTFILES_ROOT/common/git/.gitignore_global"
 
 # 環境ごとの設定を読み込む
