da5fddac4d9eb290804768d0610f5c683624faa0
taiseiue <taiseiue@wsnet.jp>
2025-06-10 10:48:44 +0900

    .macosをinstallで適用できるようにする

--- darwin/install.sh
+++ darwin/install.sh
@@ -5,3 +5,12 @@ for file in $(find $DOTFILES_ROOT/darwin/aquaskk -type f); do
 done
 
 ln -f -s $DOTFILES_ROOT/darwin/brewfile ~/.config/brewfile
+
+echo -n "Would you like to import your .macos? [y/N]: "
+read ANS
+
+case $ANS in
+  [Yy]* )
+    source $DOTFILES_ROOT/.macos
+    ;;
+esac
