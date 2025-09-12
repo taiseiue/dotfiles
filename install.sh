5313bc4ce7c0c5be64618011cb635c7f8c4ed348
taiseiue <taiseiue@wsnet.jp>
2025-09-13 01:20:22 +0900

    githooks

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ export DOTFILES_ROOT="$(cd "$(dirname "$0")" && pwd)"
 echo "source $DOTFILES_ROOT/common/zsh/.zshrc" >> ~/.zshrc
 echo "source $DOTFILES_ROOT/common/zsh/.zshenv" >> ~/.zshenv
 
-ln -f -s "$DOTFILES_ROOT/common/git" ~/.config/git
-git config --global core.excludesfile "$DOTFILES_ROOT/common/git/ignore"
+ln -i -s "$DOTFILES_ROOT/common/git" ~/.config/git
+git config --global include.path "~/.config/git/.gitconfig" # gitconfigをinclude
 
 # 環境ごとの設定を読み込む
 if [ "$(uname)" = 'Darwin' ]; then
