dea9d73f615388fb1179b56e11e0a0ef9cd1d1f8
taiseiue <taiseiue@wsnet.jp>
2025-11-30 16:50:38 +0900

    fix: streamline session variable management for macOS

--- home.nix
+++ home.nix
@@ -37,27 +37,10 @@
   home.file = {
     ".zshrc_local".source = ./zsh/.zshrc;
   };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/taiseiue/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
+  
+  home.sessionVariables = pkgs.lib.mkIf isMac {
+    SSH_AUTH_SOCK="~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
--- modules/zsh/zsh.nix
+++ modules/zsh/zsh.nix
@@ -7,15 +7,10 @@
     enableCompletion = true;
     syntaxHighlighting.enable = true;
 
-    envExtra = ''
-    if [ "$(uname)" = 'Darwin' ]; then
-      export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
-    fi
-    '';
-  initContent = ''
-    if [ -f "$HOME/.zshrc_local" ]; then
-      source "$HOME/.zshrc_local"
-    fi
-    '';
-  };
+    initContent = ''
+      if [ -f "$HOME/.zshrc_local" ]; then
+        source "$HOME/.zshrc_local"
+      fi
+      '';
+    };
 }
