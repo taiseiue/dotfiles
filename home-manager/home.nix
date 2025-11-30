3becccf399d603db1108f3b96ece31138b80b7db
taiseiue <taiseiue@wsnet.jp>
2025-11-30 16:47:15 +0900

    大改造

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -38,6 +38,7 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    "~/.config/wsoft/otameshi.toml".source = ./otameshi/otameshi.toml;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
