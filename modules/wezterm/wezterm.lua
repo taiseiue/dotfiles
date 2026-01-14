6235bdcf2e0e1a6ee075a489e2902ea47991e569
taiseiue <taiseiue@wsnet.jp>
2026-01-15 07:48:11 +0900

    やっぱりこうする

--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -12,7 +12,7 @@ config.font_size = 21
 config.color_scheme = "Tango (terminal.sexy)"
 
 -- Transparency and Blur
-config.window_background_opacity = 0.6
+config.window_background_opacity = 0.7
 config.macos_window_background_blur = 40
 
 -- Window Decorations
