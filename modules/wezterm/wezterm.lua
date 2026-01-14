d26763f23bec0853e7306e212ae0f629085bc163
taiseiue <taiseiue@wsnet.jp>
2026-01-15 07:46:59 +0900

    weztermの色をかえる

--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -12,8 +12,8 @@ config.font_size = 21
 config.color_scheme = "Tango (terminal.sexy)"
 
 -- Transparency and Blur
-config.window_background_opacity = 0.7
-config.macos_window_background_blur = 30
+config.window_background_opacity = 0.6
+config.macos_window_background_blur = 40
 
 -- Window Decorations
 config.window_decorations = "RESIZE"
