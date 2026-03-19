62e5829f7dc6975fe9c8a929cda3078b8815e081
taiseiue <taiseiue@wsnet.jp>
2026-03-19 20:05:02 +0900

    ghqしたときに出てくる一覧の長さを短くする

--- modules/zsh/zshrc.sh
+++ modules/zsh/zshrc.sh
@@ -3,14 +3,14 @@ function mkcd() {
 }
 function _ghq() {
   if [[ "$1" == "checkout" ]]; then
-      local branch=$(git branch | sed 's/^[ \*]*//' | fzf --query "$2" --prompt "branch> ")
+      local branch=$(git branch | sed 's/^[ \*]*//' | fzf  --height ~40% --query "$2" --prompt "branch> ")
       if [[ -n "$branch" ]]; then
       git checkout "$branch"
       fi
   elif [[ "$1" == "clone" || "$1" == "list" || "$1" == "rm" || "$1" == "root" || "$1" == "create" ]]; then
       command ghq "$@"
   else
-      local selected=$(ghq list | fzf --query "$1" --prompt "repo> ")
+      local selected=$(ghq list | fzf  --height ~40% --query "$1" --prompt "repo> ")
       if [[ -n "$selected" ]]; then
       cd "$(ghq root)/$selected"
       fi
