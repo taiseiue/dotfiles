6f01364c117ba88b8a7981f8735fb61aefe13e58
taiseiue <taiseiue@wsnet.jp>
2026-04-27 23:52:04 +0900

    weztermを25.11-stableにする

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,12 +11,19 @@
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    { nixpkgs, nixpkgs-stable, home-manager, ... }:
     let
       mkHome =
         system: module:
         home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                wezterm = nixpkgs-stable.legacyPackages.${system}.wezterm;
+              })
+            ];
+          };
           modules = [ module ];
         };
     in
