e7ace2e5d2718933e7fdde3ddc752e1adc5333b0
taiseiue <taiseiue@wsnet.jp>
2026-04-27 23:23:50 +0900

    fix

--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -64,7 +64,7 @@ config.keys = {
 }
 
 -- macのOptionキーをAltキーにする
-config.send_composed_key_when_left_alt_is_pressed = true
-config.send_composed_key_when_right_alt_is_pressed = true
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = false
 
 return config
