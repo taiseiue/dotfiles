674a9206cb6cdfb8a2aa7580a8629855473ad375
taiseiue <taiseiue@wsnet.jp>
2026-05-22 19:42:53 +0900

    こうなのか

--- profiles/cpp.nix
+++ profiles/cpp.nix
@@ -5,19 +5,19 @@
     ./base.nix
   ];
 
-  programs.vscode.profiles.default.extensions =
-    (with pkgs.vscode-extensions; [
-      # ms-vscode.cpp-devtools
-      ms-vscode.cpptools
-      ms-vscode.cpptools-extension-pack
-      twxs.cmake
-    ])
-    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        publisher = "ics";
-        name = "japanese-proofreading";
-        version = "1.27.1";
-        sha256 = pkgs.lib.fakeSha256;
-      }
-    ];
+ programs.vscode.profiles.default.extensions =
+  (with pkgs.vscode-extensions; [
+    # ms-vscode.cpp-devtools
+    ms-vscode.cpptools
+    ms-vscode.cpptools-extension-pack
+    twxs.cmake
+  ])
+  ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    {
+      publisher = "ICS";
+      name = "japanese-proofreading";
+      version = "1.3.0";
+      sha256 = pkgs.lib.fakeSha256;
+    }
+  ];
 }
