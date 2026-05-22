6ccd3c4f2673a77d367fdee9afd8ffd1b8bb3e61
taiseiue <taiseiue@wsnet.jp>
2026-05-23 03:52:23 +0900

    こうか

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -3,34 +3,48 @@
 {
   programs.vscode = {
     enable = true;
+    mutableExtensionsDir = false;
 
-    profiles.default.extensions =
-      (with pkgs.vscode-extensions; [
-        vscodevim.vim
-        ms-vscode-remote.remote-containers
-        github.codespaces
-        github.copilot-chat
-        github.vscode-github-actions
-        ms-vsliveshare.vsliveshare
-        streetsidesoftware.code-spell-checker
-        docker.docker
-        ms-azuretools.vscode-docker
-        ms-azuretools.vscode-containers
-        bmewburn.vscode-intelephense-client
-      ])
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          publisher = "ICS";
-          name = "japanese-proofreading";
-          version = "1.3.0";
-          sha256 = "sha256-UEEeK+PbqaEQ+kLh78NLzf8mhRx4HI9oBnn2cRjJmvc=";
-        }
-        {
-          publisher = "daylerees";
-          name = "rainglow";
-          version = "1.5.2";
-          sha256 = "sha256-1c/xQYnuJ3BkwfqjMeT2kG1ZsXyjEOypJs0pJbouZMQ=";
-        }
-      ];
-  };
+    profiles.default = {
+      userSettings = {
+        "extensions.autoUpdate" = false;
+        "extensions.autoCheckUpdates" = false;
+        "editor.formatOnSave" = true;
+        "editor.tabSize" = 2;
+        "explorer.confirmDragAndDrop" = false;
+        "git.autofetch" = false;
+        "git.confirmSync" = false;
+        "git.enableSmartCommit" = true;
+      };
+
+      extensions =
+        (with pkgs.vscode-extensions; [
+          vscodevim.vim
+          ms-vscode-remote.remote-containers
+          github.codespaces
+          github.copilot-chat
+          github.vscode-github-actions
+          ms-vsliveshare.vsliveshare
+          streetsidesoftware.code-spell-checker
+          docker.docker
+          ms-azuretools.vscode-docker
+          ms-azuretools.vscode-containers
+          bmewburn.vscode-intelephense-client
+        ])
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            publisher = "ICS";
+            name = "japanese-proofreading";
+            version = "1.3.0";
+            sha256 = "sha256-UEEeK+PbqaEQ+kLh78NLzf8mhRx4HI9oBnn2cRjJmvc=";
+          }
+          {
+            publisher = "daylerees";
+            name = "rainglow";
+            version = "1.5.2";
+            sha256 = "sha256-1c/xQYnuJ3BkwfqjMeT2kG1ZsXyjEOypJs0pJbouZMQ=";
+          }
+        ];
+      };
+};
 }
