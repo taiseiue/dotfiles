2479aa2cb1b138c2142e52f5dfd50c5f76fbf2c2
taiseiue <taiseiue@wsnet.jp>
2026-05-23 03:00:44 +0900

    ms-vscodeを導入

--- profiles/cpp.nix
+++ profiles/cpp.nix
@@ -7,7 +7,6 @@
 
  programs.vscode.profiles.default.extensions =
   (with pkgs.vscode-extensions; [
-    # ms-vscode.cpp-devtools
     ms-vscode.cpptools
     ms-vscode.cpptools-extension-pack
     twxs.cmake
@@ -18,6 +17,12 @@
       name = "japanese-proofreading";
       version = "1.3.0";
       sha256 = "sha256-UEEeK+PbqaEQ+kLh78NLzf8mhRx4HI9oBnn2cRjJmvc=";
+    }
+    {
+      publisher = "ms-vscode";
+      name = "cpp-devtools";
+      version = "0.6.2";
+      sha256 = "sha256-Wz8lg21dIRUZluVOPzlthyzxlUirTCIVU3bkLOLI2eI=";
     }
   ];
 }
