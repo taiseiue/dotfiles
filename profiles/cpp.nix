eaaca191a619582d53c4e8debfdc269a1c90e503
taiseiue <taiseiue@wsnet.jp>
2026-05-22 19:43:51 +0900

    こうか

--- profiles/cpp.nix
+++ profiles/cpp.nix
@@ -17,7 +17,7 @@
       publisher = "ICS";
       name = "japanese-proofreading";
       version = "1.3.0";
-      sha256 = pkgs.lib.fakeSha256;
+      sha256 = "sha256-UEEeK+PbqaEQ+kLh78NLzf8mhRx4HI9oBnn2cRjJmvc=";
     }
   ];
 }
