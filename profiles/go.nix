57ae052b1f4cc7fb25a06f393a1198c45f7b1fdf
taiseiue <taiseiue@wsnet.jp>
2026-06-05 10:27:01 +0900

    fix

--- profiles/go.nix
+++ profiles/go.nix
@@ -7,11 +7,4 @@
   home.packages = with pkgs; [
     go
   ];
-  programs.vscode.profiles.default.extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    publisher = 
-    name
-    version
-    sha256 = "sha256-Wz8lg21dIRUZluVOPzlthyzxlUirTCIVU3bkLOLI2eI=";
-  ];
-
 }
