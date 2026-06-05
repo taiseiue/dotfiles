0ff9dfdd5d9c03bc7a71018521ed0b34039cab51
taiseiue <taiseiue@wsnet.jp>
2026-06-05 10:26:10 +0900

    こう

--- profiles/perl.nix
+++ profiles/perl.nix
@@ -8,12 +8,13 @@
     perl
     perlcritic
   ];
-  programs.vscode.profiles.default.extensions = vscode-utils.extensionsFromVscodeMarketplace [
-    {
-      publisher = "bscan";
-      name = "perlnavigator";
-      version = "0.8.21";
-      sha256 = "sha256-UEEeK+PbqaEQ+kLh78NLzf8mhRx4HI9oBnn2cRjJmvc=";
-    }
+  programs.vscode.profiles.default.extensions =
+    pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        publisher = "bscan";
+        name = "perlnavigator";
+        version = "0.8.21";
+        sha256 = "sha256-UEEeK+PbqaEQ+kLh78NLzf8mhRx4HI9oBnn2cRjJmvc=";
+      }
   ];
 }
