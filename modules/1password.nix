23f5d6ba7e55c7125cb32c849f94b4036a249c52
taiseiue <taiseiue@wsnet.jp>
2026-06-11 01:54:48 +0900

    gitの設定変更

--- modules/1password.nix
+++ modules/1password.nix
@@ -10,4 +10,8 @@ in
   home.sessionVariables = pkgs.lib.mkIf isMac {
     SSH_AUTH_SOCK = "${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
   };
+  programs.git.settings.gpg.ssh = pkgs.lib.mkIf isMac {
+    # macOSなら、SSHは1PasswordのSSHエージェントを使う
+    program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+  };
 }
--- modules/git/git.nix
+++ modules/git/git.nix
@@ -1,65 +1,58 @@
 { config, pkgs, ... }:
 
-let
-  isMac = pkgs.stdenv.isDarwin;
-in
 {
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "taiseue";
+        email = "taiseiue@wsnet.jp";
+        signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlgPD2+ahEVUXRvZJ+DlPti2UYY1FMR4GRg5u4nX46v";
+      };
+      commit = {
+        gpgsign = true;
+      };
+      gpg = {
+        format = "ssh";
+      };
+      aliases = {
+        co = "checkout";
+        br = "branch";
+        ci = "commit";
+        st = "status";
+      };
+      core = {
+        excludesfile = "~/.config/git/ignore";
+        hookspath = "~/.config/git/hooks";
+      };
+      color = {
+        diff = "auto";
+        status = "auto";
+        branch = "auto";
+        grep = "auto";
+      };
+      url = {
+        "git@github.com:" = {
+          insteadOf = "https://github.com/";
+        };
+        pull = {
+          rebase = "false";
+        };
+        push = {
+          default = "current";
+        };
+        ghq = {
+          root = "~/source";
+        };
+      };
+    };
 
-  programs.git.settings.user = {
-    name = "taiseiue";
-    email = "taiseiue@wsnet.jp";
-    signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlgPD2+ahEVUXRvZJ+DlPti2UYY1FMR4GRg5u4nX46v";
-  };
-
-  programs.git.settings.commit = {
-    gpgsign = true;
-  };
-
-  programs.git.settings.gpg = {
-    format = "ssh";
-  };
-
-  programs.git.settings.aliases = {
-    co = "checkout";
-    br = "branch";
-    ci = "commit";
-    st = "status";
-  };
-
-  programs.git.settings.core = {
-    excludesfile = "~/.config/git/ignore";
-    hookspath    = "~/.config/git/hooks";
-  };
-
-  programs.git.settings.ghq = {
-    root = "~/source";
-  };
-  
-  programs.git.settings.color = {
-    diff   = "auto";
-    status = "auto";
-    branch = "auto";
-    grep   = "auto";
-  }; 
-
-  programs.git.settings.pull = {
-    rebase = "false";
-  };
-  programs.git.settings.push = {
-    default = "current";
-  };
-  
-  programs.git.settings.gpg.ssh =  pkgs.lib.mkIf isMac {
-    # macOSなら、SSHは1PasswordのSSHエージェントを使う
-    program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-  };
-
-  home.file = {
-    ".config/git/ignore".source = ./ignore;
-    ".config/git/hooks/pre-commit" = {
-      source = ./hooks/pre-commit.sh;
-      executable = true;
+    home.file = {
+      ".config/git/ignore".source = ./ignore;
+      ".config/git/hooks/pre-commit" = {
+        source = ./hooks/pre-commit.sh;
+        executable = true;
+      };
     };
   };
 }
