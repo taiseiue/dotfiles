d6ac7e49ccee7678c50c761d0400ace0b0fd77e7
taiseiue <taiseiue@wsnet.jp>
2026-06-12 15:27:45 +0900

    c言語の開発環境をととのえる

--- profiles/cpp.nix
+++ profiles/cpp.nix
@@ -5,24 +5,29 @@
     ./base.nix
   ];
 
- programs.vscode.profiles.default.extensions =
-  (with pkgs.vscode-extensions; [
-    ms-vscode.cpptools
-    ms-vscode.cpptools-extension-pack
-    twxs.cmake
-  ])
-  ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    {
-      publisher = "ICS";
-      name = "japanese-proofreading";
-      version = "1.3.0";
-      sha256 = "sha256-UEEeK+PbqaEQ+kLh78NLzf8mhRx4HI9oBnn2cRjJmvc=";
-    }
-    {
-      publisher = "ms-vscode";
-      name = "cpp-devtools";
-      version = "0.6.2";
-      sha256 = "sha256-Wz8lg21dIRUZluVOPzlthyzxlUirTCIVU3bkLOLI2eI=";
-    }
+  programs.vscode.profiles.default.extensions =
+    (with pkgs.vscode-extensions; [
+      ms-vscode.cpptools
+      ms-vscode.cpptools-extension-pack
+      twxs.cmake
+      vscode-extensions.llvm-vs-code-extensions.vscode-clangd
+    ])
+    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        publisher = "ICS";
+        name = "japanese-proofreading";
+        version = "1.3.0";
+        sha256 = "sha256-UEEeK+PbqaEQ+kLh78NLzf8mhRx4HI9oBnn2cRjJmvc=";
+      }
+      {
+        publisher = "ms-vscode";
+        name = "cpp-devtools";
+        version = "0.6.2";
+        sha256 = "sha256-Wz8lg21dIRUZluVOPzlthyzxlUirTCIVU3bkLOLI2eI=";
+      }
+    ];
+  programs.neovim.plugins = with pkgs.vimPlugins; [
+    coc-clangd
+    clang-tools
   ];
 }
